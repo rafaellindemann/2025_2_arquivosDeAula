f7ac22c51c886754a94bbf389811ae008752ef97
rafaellindemann <rafaelld@gmail.com>
2025-09-09 22:19:29 -0300

    att moedas

--- 2a/03-inputs/src/App.jsx
+++ 2a/03-inputs/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Demo from './components/Demo'
 import GradesCida from './components/GradesCida'
+import Moedas from './components/Moedas'
 
 function App() {
 
   return (
     <>
       <h1>Inputs</h1>
+      <Moedas />
       <GradesCida />
       <Demo />
     </>
---
+++ 2a/03-inputs/src/components/Moedas.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import './Moedas.css'
+
+function Moedas() {
+    const[real, setReal] = useState('')
+    const[dolar, setDolar] = useState('')
+    const[euro, setEuro] = useState('')
+
+    const cotacao = {
+        dolar: 5.43,
+        euro: 6.36
+    }
+
+    function converterReal(valor){
+        setReal(valor)
+        setDolar(valor/cotacao.dolar)
+    }
+
+    function converterDolar(valor){
+        setDolar(valor)
+        setReal(valor*cotacao.dolar)
+    }
+
+  return (
+    <div className='moedas-container'>
+        <h2>Moedas</h2>
+        <label htmlFor="">Real</label>
+        <input type="number" 
+            value={real}
+            onChange={(e) => converterReal(e.target.value)}
+        />
+      
+        <label htmlFor="">Dolar</label>
+        <input type="number" 
+            value={dolar}
+            onChange={(e) => converterDolar(e.target.value)}
+        />
+
+    </div>
+  )
+}
+
+export default Moedas
